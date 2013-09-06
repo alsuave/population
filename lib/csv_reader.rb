9b0b4ed130917b60120bbeb4538e6e9eaeeb1cc2
Carlos Bettaglio <cbettaglio@gmail.com>
2013-09-06 18:09:29 +0800

    fixed typos

--- lib/csv_reader.rb
+++ lib/csv_reader.rb
@@ -1,34 +1,53 @@
+# Responsible for taking in the csv and passing back each
+# area as a hash
+
 class CSVReader
   
   attr_accessor :fname, :headers
-
+  
   def initialize(filename)
     @fname = filename
   end
 
+  def read
+    f = File.new(@fname, 'r')
+    
+    # Grab the headers
+    self.headers = f.readline
+
+    # Loop over the lines
+    while (!f.eof? && next_line = f.readline)
+      values = next_line.split(',')
+      hash = create_hash(values)
+      yield(hash)
+    end
+  end
+
   def headers=(header_str)
-    @headers = header_str.split(",")
+    @headers = header_str.split(',')
 
     @headers.map! do |h|
-      
+      # remove quotes
       h.gsub!('"', '')
-
+      # remove new line
       h.strip!
-
-      h.underscore.to_sym?
+      # convert to a symbol
+      h.underscore.to_sym
     end
   end
 
   def create_hash(values)
     h = {}
     @headers.each_with_index do |header, i|
+      # remove new lines from the value
       value = values[i].strip.gsub('"', '')
-      h[header] = value unless value.empty
+      h[header] = value unless value.empty?
     end
     h
   end
 end
 
+# http://stackoverflow.com/questions/1509915/converting-camel-case-to-underscore-case-in-ruby
 class String
   def underscore
     self.gsub(/::/, '/').
@@ -36,17 +55,5 @@ class String
     gsub(/([a-z\d])([A-Z])/,'\1_\2').
     tr("-", "_").
     downcase
-  end
-end
-
-class Read
-  f = File.new(@fname , mode = "r")
-
-  self.headers = f.readline 
-
-  while (!f.eof? && next_line = f.readline)
-    value = next_line.split(",")
-    hash = create_hash(values)
-    yield(hash)
   end
 end
