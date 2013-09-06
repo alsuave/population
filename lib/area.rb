3a5d62da957e9117c1d7d907e1675394bbaf4529
Carlos Bettaglio <cbettaglio@gmail.com>
2013-09-06 18:09:16 +0800

    fixed typos

--- lib/area.rb
+++ lib/area.rb
@@ -1,15 +1,14 @@
 class Area
-  attr_accessor :zip_code, :estimated_population, :city, :state
-
+  attr_accessor :zipcode, :estimated_population, :city, :state
+     
   def initialize(hash)
-      @zip_code = hash[:zip_code].to_i || 0
-      @estimated_population = hash[:estimated_population].to_i || 0
-      @city = hash[:city] || "n/a"
-      @state = hash[:state] || "n/a"
+    @zipcode = hash[:zipcode].to_i || 0
+    @estimated_population = hash[:estimated_population].to_i || 0
+    @city = hash[:city] || "n/a"
+    @state = hash[:state] || "n/a"
   end
 
   def to_s
-    # Why does the answer not use instance variables?
-    "#{city}, #{state} #{zip_code} has #{estimated_population} people."
+    "#{city}, #{state} #{zipcode} has #{estimated_population} people."
   end
 end
